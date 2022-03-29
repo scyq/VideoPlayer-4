2ded1b427d2e5dc445557386c60a60920a510b2b
scyq <chenyuqing0429@foxmail.com>
2022-03-29 14:39:34 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Box } from "@mui/system";
+import ReactPlayer from "react-player";
+
+const urls = [
+  "videos/video3.mp4",
+  "videos/video0.mp4",
+  "videos/video2.mp4",
+  "videos/video1.mp4",
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box className="main">
+      {[0, 1].map((i) => (
+        <Box key={i} className="rowVideo">
+          {[0, 1].map((j) => (
+            <ReactPlayer
+              className="player"
+              key={`player${j}`}
+              url={urls[i * 2 + j]}
+              controls
+            ></ReactPlayer>
+          ))}
+        </Box>
+      ))}
+    </Box>
   );
 }
 
