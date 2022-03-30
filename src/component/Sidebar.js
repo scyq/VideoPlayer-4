39dfa941bf39900c5ed2b304531b3eeed5ea4ca5
scyq <chenyuqing0429@foxmail.com>
2022-03-30 17:19:52 +0800

    update ui

--- src/component/Sidebar.js
+++ src/component/Sidebar.js
@@ -1,14 +1,23 @@
 import { Box } from "@mui/system";
+import "./Sidebar.css";
 
 function Sidebar() {
   return (
-    <Box sx={{ padding: "20px" }}>
+    <Box
+      sx={{
+        width: "100%",
+        display: "flex",
+        flexDirection: "column",
+      }}
+    >
       {[0, 1, 2, 3].map((index) => (
-        <Box sx={{ background: "#F1EFF1", margin: "20px" }}>
-          <Box sx={{ color: "#3877CD", fontWeight: "bold" }}>采集信息</Box>
-          <Box>采集幅面：0</Box>
-          <Box>采集帧率：0</Box>
-          <Box>帧 号： 0</Box>
+        <Box sx={{ background: "#F1EFF1" }}>
+          <Box className="sideBarTitle">采集信息</Box>
+          <Box sx={{ padding: "10px" }}>
+            <Box className="textLine">采集幅面：0</Box>
+            <Box className="textLine">采集帧率：0</Box>
+            <Box className="textLine">帧 号： 0</Box>
+          </Box>
         </Box>
       ))}
     </Box>
